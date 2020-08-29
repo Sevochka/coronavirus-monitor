d6865fc5710e041068c851fd31bc937982cc8cae
Sevka <sevkaplay@gmail.com>
2020-08-29 21:38:50 +0300

    Change design + make cards clickable

--- src/components/MainStatistic/MainStatistic.jsx
+++ src/components/MainStatistic/MainStatistic.jsx
@@ -3,22 +3,47 @@ import { Card, Col, Row } from "antd";
 
 import "./MainStatistic.css";
 
+const KEYS = [
+    "cases",
+    "death",
+    "recovered"
+]
+
 const MainStatistic = (props) => {
 
     const numberWithCommas = (x) => {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+    const handleParentClicked = (e) => {
+        e.persist();
+        const { key } = e.target.offsetParent.dataset;
+        switch (key) {
+            case KEYS[0]:
+                alert("Был кликнут - заболевшие, ключ - "+ KEYS[0])
+                break;
+            case KEYS[1]:
+                alert("Был кликнут - смерть, ключ - "+ KEYS[1])
+                break;
+            case KEYS[2]:
+                alert("Был кликнут - выздоровевшие, ключ - "+ KEYS[2])
+                break;
+            default:
+                break;
+        }
     }
 
     const { total_cases, total_deaths, total_recovered } = props.info;
     return (
         <>
             <div className="site-card-wrapper">
-                <Row gutter={10} className="stat-row">
+                <h1 className="text-centered">За все время</h1>
+                <Row gutter={10} className="stat-row" onClick={handleParentClicked}>
                     <Col span={8} className="stat-item">
                         <Card
-                            title="Всего заболевших"
+                            title="Заболевших"
                             bordered={true}
                             className="stat-card"
+                            data-key={KEYS[0]}
                         >
                             <span className="colored-blue">
                                 {numberWithCommas(total_cases)}
@@ -30,6 +55,7 @@ const MainStatistic = (props) => {
                             title="Смертей"
                             bordered={true}
                             className="stat-card"
+                            data-key={KEYS[1]}
                         >
                             <span className="colored-red">
                                 {numberWithCommas(total_deaths)}
@@ -41,6 +67,7 @@ const MainStatistic = (props) => {
                             title="Выздоровевших"
                             bordered={true}
                             className="stat-card"
+                            data-key={KEYS[2]}
                         >
                             <span className="colored-green">
                                 {numberWithCommas(total_recovered)}
