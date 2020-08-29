45ef0cb10bfe15de84d7c854014aaa9654a5f2c1
Sevka <sevkaplay@gmail.com>
2020-08-29 16:56:39 +0300

    Change ui, add icons, add numberWithCommas method

--- src/components/MainStatistic/MainStatistic.jsx
+++ src/components/MainStatistic/MainStatistic.jsx
@@ -5,18 +5,24 @@ import "./MainStatistic.css";
 
 const MainStatistic = (props) => {
 
-    const {total_cases, total_deaths, total_recovered} = props.info;
+    const numberWithCommas = (x) => {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    const { total_cases, total_deaths, total_recovered } = props.info;
     return (
         <>
             <div className="site-card-wrapper">
                 <Row gutter={10} className="stat-row">
                     <Col span={8} className="stat-item">
                         <Card
-                            title="Заболевших"
+                            title="Всего заболевших"
                             bordered={true}
                             className="stat-card"
                         >
-                            {total_cases}
+                            <span className="colored-blue">
+                                {numberWithCommas(total_cases)}
+                            </span>
                         </Card>
                     </Col>
                     <Col span={8} className="stat-item">
@@ -25,7 +31,9 @@ const MainStatistic = (props) => {
                             bordered={true}
                             className="stat-card"
                         >
-                            {total_deaths}
+                            <span className="colored-red">
+                                {numberWithCommas(total_deaths)}
+                            </span>
                         </Card>
                     </Col>
                     <Col span={8} className="stat-item">
@@ -34,13 +42,22 @@ const MainStatistic = (props) => {
                             bordered={true}
                             className="stat-card"
                         >
-                            {total_recovered}
+                            <span className="colored-green">
+                                {numberWithCommas(total_recovered)}
+                            </span>
                         </Card>
                     </Col>
                 </Row>
             </div>
 
-            <br/>
+            <br />
+
+            {/* <Card title="Заболевшие сейчас" style={{ width: 300 }} className="stat-card">
+                <p>
+                    12000
+                </p>
+            </Card> */}
+
         </>
     );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Menu } from 'antd';
-import { MailOutlined, AppstoreOutlined } from '@ant-design/icons';
+import Icon from '@ant-design/icons';
 import { useState } from 'react';
+import VirusSvg from "../../assets/virus.svg";
+import StatSvg from "../../assets/statistics.svg";
+import InfoSvg from "../../assets/info.svg"
+
 
 import './Navbar.css';
 
@@ -14,14 +18,17 @@ const Navbar = () => {
     };
 
     return (
-        <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal" theme="light" className="nav-menu">
-            <Menu.Item key="coronavirus" className="menu-item" icon={<MailOutlined />}>
-                <span>Коронавирус</span>
+        <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal" theme="dark" className="nav-menu">
+            <Menu.Item key="coronavirus" className="menu-item">
+                <img src={VirusSvg} alt="Virus icon" className="icon" />
+                Коронавирус
             </Menu.Item>
-            <Menu.Item key="stat" className="menu-item" icon={<AppstoreOutlined />}>
+            <Menu.Item key="stat" className="menu-item" >
+                <img src={StatSvg} alt="Stat icon" className="icon" />
                 Статистика
             </Menu.Item>
-            <Menu.Item key="info" className="menu-item" icon={<AppstoreOutlined />}>
+            <Menu.Item key="info" className="menu-item" >
+                <img src={InfoSvg} alt="Information icon" className="icon" />
                 Информация
             </Menu.Item>
         </Menu>
