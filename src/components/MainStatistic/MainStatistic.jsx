dd7e905c48744064a53c074179c0182437c4d4e1
Sevka <sevkaplay@gmail.com>
2020-09-02 11:57:32 +0300

    Make info scrollable

--- src/components/MainStatistic/MainStatistic.jsx
+++ src/components/MainStatistic/MainStatistic.jsx
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types';
 
 import './MainStatistic.css';
 
-const MainStatistic = (props) => {
-  const numberWithCommas = (x) => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 
 const MainStatistic = props => {
     const numberWithCommas = x => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
@@ -17,46 +15,57 @@ const MainStatistic = props => {
         total_serious_cases
     } = props.info;
 
-        <div className="stat-cards-wrapper">
-          <section className="main-stat__card">
-            <div className="card__part">
-              <span className="part__number number-infected">{numberWithCommas(totalCases)}</span>
-              <span className="part__title">Выявлено заболевших</span>
-            </div>
-            <div className="card__part">
-              <span className="part__number number-infected">{numberWithCommas(total_new_cases_today)}</span>
-              <span className="part__title">Выявлено заболевшихза сутки</span>
-            </div>
-          </section>
+    return (
+        <>
+            <article className="main-stat">
+                <h2 className="main-stat__header">Статистика за все время</h2>
 
-          <section className="main-stat__card">
-            <div className="card__part">
-              <span className="part__number number-death">{numberWithCommas(total_deaths)}</span>
-              <span className="part__title">Человек умерло</span>
-            </div>
-            <div className="card__part">
-              <span className="part__number number-death">{numberWithCommas(total_new_deaths_today)}</span>
-              <span className="part__title">Человека умерло за сутки</span>
-            </div>
-          </section>
+                <div className="stat-cards-wrapper">
+                    <section className="main-stat__card">
+                        <div className="card__part">
+                            <span className="part__number number-infected">{numberWithCommas(total_cases)}</span>
+                            <span className="part__title">Выявлено заболевших</span>
+                        </div>
+                        <div className="card__part">
+                            <span className="part__number number-infected">{numberWithCommas(total_new_cases_today)}</span>
+                            <span className="part__title">Выявлено заболевшихза сутки</span>
+                        </div>
+                    </section>
 
-          <section className="main-stat__card">
-            <div className="card__part">
-              <span className="part__number number-recovered">{numberWithCommas(total_recovered)}</span>
-              <span className="part__title">Человек выздоровело</span>
-            </div>
-            <div className="card__part">
-              <span className="part__number number-serious">{numberWithCommas(total_serious_cases)}</span>
-              <span className="part__title">В тяжелом состоянии</span>
-            </div>
-          </section>
-        </div>
-      </article>
+                    <section className="main-stat__card">
+                        <div className="card__part">
+                            <span className="part__number number-death">{numberWithCommas(total_deaths)}</span>
+                            <span className="part__title">Человек умерло</span>
+                        </div>
+                        <div className="card__part">
+                            <span className="part__number number-death">{numberWithCommas(total_new_deaths_today)}</span>
+                            <span className="part__title">Человека умерло за сутки</span>
+                        </div>
+                    </section>
 
-      <br />
+                    <section className="main-stat__card">
+                        <div className="card__part">
+                            <span className="part__number number-recovered">{numberWithCommas(total_recovered)}</span>
+                            <span className="part__title">Человек выздоровело</span>
+                        </div>
+                        <div className="card__part">
+                            <span className="part__number number-serious">{numberWithCommas(total_serious_cases)}</span>
+                            <span className="part__title">В тяжелом состоянии</span>
+                        </div>
+                    </section>
+                </div>
+            </article>
 
-    </>
-  );
+            <br />
+
+            {/* <Card title="Заболевшие сейчас" style={{ width: 300 }} className="stat-card">
+                <p>
+                    12000
+                </p>
+            </Card> */}
+
+        </>
+    );
 };
 
 MainStatistic.propTypes = {
