7c02f3ceeeb810fa270752c64540a09e46b7ddce
Green <metallgreen@gmail.com>
2020-09-03 14:56:28 +0300

    routeMaps foreach -> reduce, prettier eol: CRLF

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,42 +4,40 @@ import Statistic from 'pages/Statistic';
 import Main from 'pages/Main';
 import Page404 from 'pages/Error404';
 
-const routes = [{
-  name: 'home',
-  url: '/',
-  component: Main,
-  exact: true,
-},
-{
-  name: 'statistic',
-  url: '/stat',
-  component: Statistic,
-  exact: true,
-},
-{
-  name: 'information',
-  url: '/info',
-  component: Information,
-  exact: true,
-},
-{
-  name: 'country statistic',
-  url: '/country/:id',
-  component: CountryPage,
-},
-{
-  url: '**',
-  component: Page404,
-},
+const routes = [
+  {
+    name: 'home',
+    url: '/',
+    component: Main,
+    exact: true,
+  },
+  {
+    name: 'statistic',
+    url: '/stat',
+    component: Statistic,
+    exact: true,
+  },
+  {
+    name: 'information',
+    url: '/info',
+    component: Information,
+    exact: true,
+  },
+  {
+    name: 'country statistic',
+    url: '/country/:id',
+    component: CountryPage,
+  },
+  {
+    url: '**',
+    component: Page404,
+  },
 ];
 
-const routesMap = {};
-
-routes.forEach((route) => {
-  if (Object.prototype.hasOwnProperty.call(route, 'name')) {
-    routesMap[route.name] = route.url;
-  }
-});
+const routesMap = routes.reduce((prevRoutes, currentRoute) => ({
+  ...prevRoutes,
+  [currentRoute.name]: currentRoute.url,
+}), {});
 
 export default routes;
 export {
