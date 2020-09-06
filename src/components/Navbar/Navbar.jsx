e43a374180f0d998ada700b178ed4b487abf7d7d
Sevka <sevkaplay@gmail.com>
2020-09-06 23:09:32 +0300

    Change navbar behaviour

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,41 +1,28 @@
-import React, { useState } from 'react';
-import { Menu } from 'antd';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { routesMap } from 'routes';
+
 import VirusSvg from 'assets/virus.svg';
 import StatSvg from 'assets/statistics.svg';
 import InfoSvg from 'assets/info.svg';
+
 import './Navbar.scss';
 
-const Navbar = () => {
-  const [current, setCurrent] = useState('mail');
-
-  const handleClick = (e) => {
-    setCurrent(e.key);
-  };
-
-  return (
-    <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal" className="nav-menu" theme="dark">
-      <Menu.Item key="coronavirus" className="nav-menu__item">
-        <Link to={routesMap.home}>
-          <img src={VirusSvg} alt="Virus icon" className="nav-menu__icon" />
-          Коронавирус
-        </Link>
-      </Menu.Item>
-      <Menu.Item key="stat" className="nav-menu__item">
-        <Link to={routesMap.statistic}>
-          <img src={StatSvg} alt="Stat icon" className="nav-menu__icon" />
-          Статистика
-        </Link>
-      </Menu.Item>
-      <Menu.Item key="info" className="nav-menu__item">
-        <Link to={routesMap.information}>
-          <img src={InfoSvg} alt="Information icon" className="nav-menu__icon" />
-          Информация
-        </Link>
-      </Menu.Item>
-    </Menu>
-  );
-};
+const Navbar = () => (
+  <nav className="nav-menu">
+    <NavLink exact to={routesMap.home} key="coronavirus" className="nav-menu__item" activeClassName="nav-menu-item-selected">
+      <img src={VirusSvg} alt="Virus icon" className="nav-menu__icon" />
+      Коронавирус
+    </NavLink>
+    <NavLink exact to={routesMap.statistic} key="stat" className="nav-menu__item" activeClassName="nav-menu-item-selected">
+      <img src={StatSvg} alt="Stat icon" className="nav-menu__icon" />
+      Статистика
+    </NavLink>
+    <NavLink exac to={routesMap.information} key="info" className="nav-menu__item" activeClassName="nav-menu-item-selected">
+      <img src={InfoSvg} alt="Information icon" className="nav-menu__icon" />
+      Информация
+    </NavLink>
+  </nav>
+);
 
 export default Navbar;
