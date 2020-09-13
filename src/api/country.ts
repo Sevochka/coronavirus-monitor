4cd97cf607193e80f3362c48e2892b57e38050fe
Sevka <sevkaplay@gmail.com>
2020-09-13 15:23:51 +0300

    Fix api

--- src/api/country.ts
+++ src/api/country.ts
@@ -1,54 +1,50 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 import { IMainStat } from 'interfaces/IMainStat';
 import { ICountryMainStat } from 'interfaces/ICountryMainStat';
-import { ICountryTimelineStat } from "interfaces/ICountryTimelineStat";
-import { ICountryTotalStat } from "interfaces/ICountryTotalStat";
+import { ICountryTimelineStat } from 'interfaces/ICountryTimelineStat';
+import { ICountryTotalStat } from 'interfaces/ICountryTotalStat';
 
 const mainUrl = 'https://whispering-bastion-66552.herokuapp.com/api/';
 
-const loadGlobalStat = () => {
-  axios
-    .get<IMainStat>(`${mainUrl}global-stats`)
+const loadGlobalStat = (): Promise<AxiosResponse<IMainStat> | Error> => {
+  return axios
+    .get(`${mainUrl}global-stats`)
     .then((response) => {
       return response;
     })
-    .catch(error => {
-      console.log(error);
-    });
+    .catch((error: Error) => error);
 };
 
-const loadAllCountryStat = () => {
-  axios
-    .get<ICountryMainStat[]>(`${mainUrl}country-totals`)
+const loadAllCountryStat = (): Promise<AxiosResponse<ICountryMainStat[]> | Error> => {
+  return axios
+    .get(`${mainUrl}country-totals`)
     .then((response) => {
       return response;
     })
-    .catch(error => {
-      console.log(error);
-    });
+    .catch((error: Error) => error);
 };
 
-const loadCountryTotalStat = (countryCode: string) => {
-  axios
-    .get<ICountryTotalStat>(`${mainUrl}country-totals/${countryCode}`)
+const loadCountryTotalStat = (
+  countryCode: string,
+): Promise<AxiosResponse<ICountryTotalStat> | Error> => {
+  return axios
+    .get(`${mainUrl}country-totals/${countryCode}`)
     .then((response) => {
       return response;
     })
-    .catch(error => {
-      console.log(error);
-    });
+    .catch((error: Error) => error);
 };
 
-const loadCountryTimelineStat = (id: string) => {
-  axios
-    .get<ICountryTimelineStat>(`${mainUrl}country-timeline/${id}`)
+const loadCountryTimelineStat = (
+  id: string,
+): Promise<AxiosResponse<ICountryTimelineStat> | Error> => {
+  return axios
+    .get(`${mainUrl}country-timeline/${id}`)
     .then((response) => {
       return response;
     })
-    .catch(error => {
-      console.log(error);
-    });
+    .catch((error: Error) => error);
 };
 
 export { loadGlobalStat, loadAllCountryStat, loadCountryTotalStat, loadCountryTimelineStat };
